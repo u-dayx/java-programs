cf7776b6ec185c0d26ad52bc41d631f94643a86c
Uday H <hemarajuuday@gmail.com>
2026-09-19 10:50:17 +0530

    swapping two variables using third variable

---
+++ p10.java
@@ -0,0 +1,15 @@
+
+public class p10 {
+    public static void main (String[]args){
+        String  ai =" hii ";
+        String  ui ="hello ";
+        String temp ;
+        temp = ai;
+        ai = ui ;
+        ui = temp;
+        System.out.println(" the varible ai after swapping is :" + ai);
+        System.out.println("the variable ui afte swapping is :" + ui);
+
+    }
+    
+}
